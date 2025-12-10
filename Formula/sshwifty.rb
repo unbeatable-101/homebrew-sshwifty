9e60e374a17e2302bf1a22c4991f7d2c880712ca
William Davis <w4davis@uwaterloo.ca>
2025-12-10 01:27:09 -0500

    Fix sshwifty formula for modern Homebrew (remove std_npm_args)

--- Formula/sshwifty.rb
+++ Formula/sshwifty.rb
@@ -1,10 +1,11 @@
 require "language/node"
+
 class Sshwifty < Formula
   desc "Web SSH & Telnet"
   homepage "https://github.com/nirui/sshwifty"
   url "https://github.com/nirui/sshwifty.git",
-    tag:      "0.4.2-beta-release-prebuild",
-    revision: "f3e550d4358f8ad3959c8cc2b77ce28b0d74c954"
+      tag:      "0.4.2-beta-release-prebuild",
+      revision: "f3e550d4358f8ad3959c8cc2b77ce28b0d74c954"
   license "AGPL-3.0-or-later"
 
   livecheck do
@@ -12,22 +13,24 @@ class Sshwifty < Formula
     regex(/(\d+(?:\.\d+)+-beta-release)/i)
   end
 
-  bottle do
-    root_url "https://github.com/unbeatable-101/homebrew-sshwifty/releases/download/sshwifty-0.3.6-beta"
-    sha256 cellar: :any_skip_relocation, ventura: "6d93064e715a0ed8c11868af15b5254036ee225895357d71bf54adef239aa6ae"
-  end
-
   depends_on "go" => :build
   depends_on "node" => :build
 
   def install
-    # Changing one of the default paths searched for config file to homebrew's etc folder
-    inreplace "application/configuration/loader_file.go", "/etc/sshwifty.conf.json", "#{etc}/sshwifty/sshwifty.conf" \
-                                                                                     ".json"
-    system "npm", "install", *Language::Node.std_npm_args
+    # Patch config search path into Homebrew's etc
+    inreplace "application/configuration/loader_file.go",
+              "/etc/sshwifty.conf.json",
+              "#{etc}/sshwifty/sshwifty.conf.json"
+
+    # Build frontend (formerly using std_npm_args)
+    system "npm", "install"
     system "npm", "run", "build"
-    bin.install "sshwifty"
-    mkdir "#{etc}/sshwifty"
+
+    # Build Go backend
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/sshwifty"
+
+    # Install config
+    (etc/"sshwifty").mkpath
     etc.install "sshwifty.conf.example.json" => "sshwifty/sshwifty.conf.json"
   end
 
@@ -43,7 +46,9 @@ class Sshwifty < Formula
     <<~EOS
       A sample configuration file has been installed at:
         #{etc}/sshwifty/sshwifty.conf.json
-      Please edit this file in order to properly configure your installation, see https://github.com/nirui/sshwifty for more info on the settings available.
+
+      Edit this file to configure your installation.
+      See https://github.com/nirui/sshwifty for documentation.
     EOS
   end
 
@@ -68,12 +73,15 @@ class Sshwifty < Formula
         ]
       }
     EOS
+
     fork do
       ENV["SSHWIFTY_CONFIG"] = testpath/"sshwifty.conf.json"
-      system bin/"sshwifty"
+      exec bin/"sshwifty"
     end
+
     sleep 5
-    assert_match "1", shell_output("curl -s http://localhost:#{port}|grep -c \"Also, surely you smart people " \
-                                   "knows that application such like this one require JavaScript to run :)\"")
+    assert_match "1", shell_output(
+      "curl -s http://localhost:#{port} | grep -c \"require JavaScript to run\""
+    )
   end
 end
